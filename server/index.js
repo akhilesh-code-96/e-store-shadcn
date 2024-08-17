88802ff907588169a9755503e34c0a827253de29
akhilesh-code-96 <srivastavakhilesh157@gmail.com>
2024-08-18 01:43:30 +0530

    push

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(
   cors({
     origin: ["https://twirl-one.vercel.app"],
-    methods: ["POST", "GET"],
+    methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
 );
